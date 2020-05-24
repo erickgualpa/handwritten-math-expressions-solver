32ed7d0b55fc99e00f0bcda6ea6a36cf3298c53f
Erick GM <m.erickgualpa@gmail.com>
2020-05-24 12:32:56 +0200

    ADD: Exception handling, ADD: Some testing images, ADD: Save results images

--- main.py
+++ main.py
@@ -12,7 +12,8 @@ with open(DIGITS_SYMBOLS_MAPPING, "r") as dig_sym_mapping_file:
     labels_mapping = json.load(dig_sym_mapping_file)["INV_DIGITS_SYMBOLS_MAPPING"]
 
 ## LOAD IMAGES FOR TESTING ########################################
-path_img = './img/'
+path_img = './testing_imgs/'
+path_results_img = './testing_imgs_results/'
 
 working_ims = []
 for root, dirs, files in os.walk(path_img):
@@ -20,13 +21,78 @@ for root, dirs, files in os.walk(path_img):
         working_ims.append(loadImage(path_img + filename))
 ###################################################################
 
-## LOAD CLASSIFIER ################################################
-clf = load_model('./classifiers/digits_symbols_cnn_classif.h5')
-###################################################################
+try:
+    ## LOAD CLASSIFIER ################################################
+    clf = load_model('./classifiers/digits_symbols_cnn_classif_128_4.h5.h5')
+    ###################################################################
+    try:
+        ## SET WORKING IMAGE ##############################################
+        im_index = 2
+        working_im = working_ims[im_index]
+        showImage(resizeImage(working_im, 0.3))
+        ###################################################################
 
-## SET WORKING IMAGE ##############################################
-working_im = working_ims[1]
-showImage(resizeImage(working_im, 0.3))
+        ## TEXT DETECTION #################################################
+        boxes, im_detection = detect_text_on_image(working_im, 0.8)    # (image, min_confidence)
+        # showImage(resizeImage(im_detection, 30))
+        detected_texts = get_boxes_as_images(boxes, working_im)    # Extract de bounding rectangles of detected text as images
+        ###################################################################
+
+        ## DIGITS AND SYMBOLS EXTRACTION AS IMAGES ########################
+        expression_list = []
+        expression = []
+        for im_text in detected_texts:
+            detections, _ = process_image(im_text.copy())
+            detected_digits_symbols = get_boxes_as_images(detections, im_text)
+            # showImage(resizeImage(im_contours, 30))
+            math_exp = []    # math_exp -> Symbol list
+            for im_digit_symbol in detected_digits_symbols:
+                # Convert 'im_digit_symbol' to grayscale
+                im_gray = cv2.cvtColor(im_digit_symbol, cv2.COLOR_BGR2GRAY)
+                # showImage(resizeImage(im_gray, 30))
+
+                im_gray = pre_classification_image_processing(im_gray)
+                showImage(resizeImage(im_gray, 0.3))
+
+                # Resize image to (28,28)
+                im_gray = cv2.resize(im_gray, (28, 28), interpolation=cv2.INTER_AREA)
+
+                # Reshape Image to (28,28,1)
+                im_gray = im_gray.reshape(28, 28, 1)
+
+                # Predict expression symbol
+                symbol = clf.predict_classes(np.array([im_gray]))
+
+                # Append symbol on math expression
+                math_exp.append(labels_mapping[str(symbol[0])])
+            expression_list.append(np.array(math_exp))
+        expression_list = np.array(expression_list)
+        ###################################################################
+
+        ## EXPRESSION SOLVING ############################################
+        im_result = working_im.copy()
+        message_height_offset = 100
+        for (exp, box) in zip(expression_list, boxes):
+            str_exp, result = solve_expression(exp)
+            print(str_exp, '=', result)
+            # Show expression result on image
+            x_pos = box[0]
+            y_pos = (box[3] + message_height_offset) if (box[3] + message_height_offset) < im_result.shape[0] else im_result.shape[0]
+            im_result = write_message_on_img(im_result, str(str_exp) + '=' + str(result), position=(x_pos, y_pos))
+        showImage(resizeImage(im_result, 0.3))
+        ##################################################################
+
+        ## SAVE IMAGE RESULT #############################################
+        im_res_filename = path_results_img + str(im_index) + '_result.jpg'
+        cv2.imwrite(im_res_filename, im_result)
+        ##################################################################
+    except IndexError:
+        print("[ERROR]: Working image not found")
+except OSError:
+    print('[ERROR]: Classifier filename not found')
+
+print("[INFO]: Finishing program...")
+
 
 """
 vid = cv2.VideoCapture(0)
@@ -39,58 +105,5 @@ while(True):
 vid.release()
 cv2.destroyAllWindows()
 """
-
-###################################################################
-
-## TEXT DETECTION #################################################
-boxes, im_detection = detect_text_on_image(working_im, 0.8)    # (image, min_confidence)
-# showImage(resizeImage(im_detection, 30))
-detected_texts = get_boxes_as_images(boxes, working_im)    # Extract de bounding rectangles of detected text as images
-###################################################################
-
-## DIGITS AND SYMBOLS EXTRACTION AS IMAGES ########################
-expression_list = []
-expression = []
-for im_text in detected_texts:
-    detections, _ = process_image(im_text.copy())
-    detected_digits_symbols = get_boxes_as_images(detections, im_text)
-    # showImage(resizeImage(im_contours, 30))
-    math_exp = []    # math_exp -> Symbol list
-    for im_digit_symbol in detected_digits_symbols:
-        # Convert 'im_digit_symbol' to grayscale
-        im_gray = cv2.cvtColor(im_digit_symbol, cv2.COLOR_BGR2GRAY)
-        # showImage(resizeImage(im_gray, 30))
-
-        im_gray = pre_classification_image_processing(im_gray)
-        showImage(resizeImage(im_gray, 0.3))
-
-        # Resize image to (28,28)
-        im_gray = cv2.resize(im_gray, (28, 28), interpolation=cv2.INTER_AREA)
-
-        # Reshape Image to (28,28,1)
-        im_gray = im_gray.reshape(28, 28, 1)
-
-        # Predict expression symbol
-        symbol = clf.predict_classes(np.array([im_gray]))
-
-        # Append symbol on math expression
-        math_exp.append(labels_mapping[str(symbol[0])])
-    expression_list.append(np.array(math_exp))
-expression_list = np.array(expression_list)
-###################################################################
-
-## EXPRESSION SOLVING ############################################
-im_result = working_im.copy()
-message_height_offset = 100
-for (exp, box) in zip(expression_list, boxes):
-    str_exp, result = solve_expression(exp)
-    print(str_exp, '=', result)
-    # Show expression result on image
-    x_pos = box[0]
-    y_pos = (box[3] + message_height_offset) if (box[3] + message_height_offset) < im_result.shape[0] else im_result.shape[0]
-    im_result = write_message_on_img(im_result, str(str_exp) + '=' + str(result), position=(x_pos, y_pos))
-im_result = resizeImage(im_result, 0.3)
-showImage(im_result)
-##################################################################
 
 
